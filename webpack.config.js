2a2e6d3bd9e3cf8a95cb91ad6aac6a2eb6fa4a60
jan K. Pluta <jkpluta@gmail.com>
2017-08-23 09:18:20 +0200

    Nieudany eksperyment Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const path = require('path');
 
-module.exports = {
-  entry: {
-    'renderer': './renderer.js'
+module.exports = [
+  {
+    entry: {
+      'renderer': './renderer.js'
+    },
+    target: 'electron-renderer',
+    output: {
+      path: path.resolve(__dirname, './webpack'),
+      filename: 'renderer.js',
+      library: 'renderer',
+      libraryTarget: 'umd'
+    }
   },
-  target: 'electron-renderer',
-  output: {
-    path: path.resolve(__dirname, './webpack'),
-    filename: 'renderer.js',
-    library: 'renderer',
-    libraryTarget: 'umd'
+  {
+    entry: {
+      'main': './main.js'
+    },
+    target: 'electron-main',
+    output: {
+      path: path.resolve(__dirname, './webpack'),
+      filename: 'main.js'
+    }
   }
-};
+];
