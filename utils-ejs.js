c11a6b9975f21f3895f111ba3634161c8b38dd7a
jan K. Pluta <jkpluta@gmail.com>
2017-08-24 15:41:19 +0200

    Eksperymentalny webpack

--- utils-ejs.js
+++ utils-ejs.js
@@ -3,15 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var ejs = require("ejs");
 var fs = require("fs");
 var path = require("path");
-function ejsToHtml(target, template, name, data) {
-    var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'utils-ejs.json'), 'utf8'));
-    console.log(config);
-    var src = template + '.ejs';
-    var dst = path.join(target, name) + '.html';
+function ejsToHtml(target) {
+    var ejsPages = JSON.parse(fs.readFileSync(path.join(__dirname, 'utils-ejs.json'), 'utf8'));
+    console.log(ejsPages);
     var opts = { root: __dirname, cache: false, filename: path.join(__dirname, ',') };
-    var tmplt = fs.readFileSync(src, 'utf8');
-    var html = ejs.render(tmplt, data, opts);
-    fs.writeFileSync(dst, html, { encoding: 'utf-8' });
+    for (var ejsName in ejsPages) {
+        console.log('-->');
+        console.log(ejsName);
+        var ejsPage = ejsPages[ejsName];
+        var src = ejsPage.template + '.ejs';
+        var dst = path.join(__dirname, target, ejsName) + '.html';
+        var tmplt = fs.readFileSync(src, 'utf8');
+        var html = ejs.render(tmplt, ejsPage, opts);
+        fs.writeFileSync(dst, html, { encoding: 'utf-8' });
+    }
 }
 exports.ejsToHtml = ejsToHtml;
 //# sourceMappingURL=utils-ejs.js.map
--- utils-ejs.ts
+++ utils-ejs.ts
@@ -2,15 +2,21 @@ import ejs = require('ejs');
 import fs = require('fs');
 import path = require('path');
 
-export function ejsToHtml(target, template, name, data)
+export function ejsToHtml(target)
 {
-    var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'utils-ejs.json'), 'utf8'))
-    console.log(config)
+    var ejsPages = JSON.parse(fs.readFileSync(path.join(__dirname, 'utils-ejs.json'), 'utf8'))
+    console.log(ejsPages)
 
-    var src = template + '.ejs';
-    var dst = path.join(target, name) + '.html';
     var opts = { root: __dirname, cache: false, filename: path.join(__dirname, ',') };
-    var tmplt = fs.readFileSync(src, 'utf8')
-    var html = ejs.render(tmplt, data, opts)
-    fs.writeFileSync(dst, html, { encoding: 'utf-8'})
+    for(var ejsName in ejsPages) {
+        console.log('-->')
+        console.log(ejsName)
+        var ejsPage = ejsPages[ejsName]
+        var src = ejsPage.template + '.ejs';
+        var dst = path.join(__dirname, target, ejsName) + '.html';
+        var tmplt = fs.readFileSync(src, 'utf8')
+        var html = ejs.render(tmplt, ejsPage, opts)
+
+        fs.writeFileSync(dst, html, { encoding: 'utf-8'})
+    }
 }
