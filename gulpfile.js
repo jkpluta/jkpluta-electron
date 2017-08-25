e8b36ed6d89d033cb7031cb31148105e1cb3e569
jan K. Pluta <jkpluta@gmail.com>
2017-08-25 09:03:12 +0200

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,21 @@ var opts = { cache: false } //{ root: __dirname, cache: false, filename: path.jo
 var ejsPages = JSON.parse(fs.readFileSync('utils-ejs.json', 'utf8'))
 
 gulp.task('default', function() {
+
+    try {
+        fs.mkdirSync('app')
+    } catch (err) {
+    }
+    
+    var package = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+    var app = JSON.parse(fs.readFileSync('package-app.json', 'utf8'));
+    app.name = package.name;
+    app.version = package.version;
+    app.description = package.description;
+    app.author = package.author;
+    app.license = package.license;
+    fs.writeFileSync('app/package.json', JSON.stringify(app), { encoding: 'utf8'})
+      
     gulp.src("node_modules/bootstrap/dist/css/*.min.css")
     .pipe(gulp.dest("css"))
     .pipe(gulp.dest("app/css"));
@@ -25,9 +40,8 @@ gulp.task('default', function() {
     gulp.src("img/*")
     .pipe(gulp.dest("app/img"));
 
-    gulp.src("package-app.json")
-    .pipe(rename("package.json"))
-    .pipe(gulp.dest("app"));
+    gulp.src("build/*")
+    .pipe(gulp.dest("app/build"));
 
     for(var ejsName in ejsPages) {
         var ejsPage = ejsPages[ejsName]
