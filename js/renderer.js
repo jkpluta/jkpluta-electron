467ff6127c9a1f7ed42727739dba7c1b99a8c097
Jan K. Pluta <jkpluta@gmail.com>
2017-09-11 18:52:17 +0200

    Strona główna

--- js/renderer.js
+++ js/renderer.js
@@ -50,18 +50,56 @@ function start(sel, spnr, href, func) {
     });
 }
 exports.start = start;
-function updateMain(sel, html) {
-    var dom = $(html);
-    dom.find('#header').remove();
-    $(sel).html(dom);
-    $(sel).find('a').click(function () {
-        loadUrl($(this).attr('href'));
-        return false;
-    });
+function updateMainInfo(sel, html) {
+    $('#info').html(html);
 }
-exports.updateMain = updateMain;
+exports.updateMainInfo = updateMainInfo;
+function updateMainBookmarks(sel, html) {
+    $(sel).html('');
+    var listy = $('dl', html);
+    for (var i = 0; i < listy.length; i++) {
+        $(sel).append('<div id="bk' + i + '" class="col-sm-6 col-md-4 col-lg-3"></div>');
+        var bieżąca_lista = listy.eq(i);
+        var bieżące_pozycje = bieżąca_lista.children('dt');
+        $('#bk' + i).append('<h4>' + bieżąca_lista.prev().html() + '</h4>');
+        $('#bk' + i).append('<p><dl>');
+        for (var j = 0; j < bieżące_pozycje.length; j++) {
+            var bieżąca_pozycja = bieżące_pozycje.eq(j);
+            var bieżące_linki = bieżąca_pozycja.children('a');
+            if (bieżące_linki.length > 0) {
+                var link = bieżące_linki.first();
+                if (link.attr('ICON_URI') != null)
+                    $('#bk' + i).append('<dt><a href="' + link.attr('href') + '"><img src="' + link.attr('ICON_URI') + '" alt="" title="' + link.text() + '" width="16" height="16"> ' + link.text() + '</a></dt>');
+                else
+                    $('#bk' + i).append('<dt>' + link[0].outerHTML + '</dt>');
+            }
+        }
+        $('#bk' + i).append('</dl></p>');
+    }
+    $(sel).find('a').attr('target', '_blank');
+}
+exports.updateMainBookmarks = updateMainBookmarks;
+function updateMainIcons(sel, html) {
+    $(sel).html('');
+    var links = $('a[icon], a[icon_uri]', html);
+    $(sel).append('<p>');
+    for (var i = 0; i < links.length; i++) {
+        var link = links.eq(i);
+        if (link.attr('ICON_URI') != null)
+            $(sel).append('<a href="' + link.attr('href') + '"><img src="' + link.attr('ICON_URI') + '" alt="' + link.text() + '" title="' + link.text() + '" width="32" height="32"></a> ');
+        else
+            $(sel).append('<a href="' + link.attr('href') + '"><img src="' + link.attr('ICON') + '" alt="' + link.text() + '" title="' + link.text() + '" width="32" height="32"></a> ');
+    }
+    $(sel).append('</p>');
+    $(sel).find('a').attr('target', '_blank');
+}
+exports.updateMainIcons = updateMainIcons;
 function startMain(href) {
-    start("#main", "#spnnr", href, updateMain);
+    start('#info', '#info', '/info.html', updateMainInfo);
+    start('#icns', '#icns', '/icons.html', updateMainIcons);
+    start('#bks', '#bke', '/bookmarks.html', updateMainBookmarks);
+    $('#google').focus();
+    // start("#main", "#spnnr", href, updateMain)
 }
 exports.startMain = startMain;
 window.startMain = startMain;
--- ts/renderer.ts
+++ ts/renderer.ts
@@ -49,19 +49,58 @@ export function start(sel: string | JQuery<HTMLElement>, spnr: string | JQuery<H
         }
     });
 }
-export function updateMain(sel: string | JQuery<HTMLElement>, html: any): void 
+export function updateMainInfo(sel: string | JQuery<HTMLElement>, html: any): void 
 {
-    var dom = $(html);
-    dom.find('#header').remove();
-    $(sel).html(<any>dom);
-    $(sel).find('a').click(function () {
-        loadUrl($(this).attr('href'));
-        return false;
-    });
+    $('#info').html(html)
+}
+export function updateMainBookmarks(sel: string | JQuery<HTMLElement>, html: any): void 
+{
+    $(sel).html('');
+    var listy = $('dl', html);
+    for (var i = 0; i < listy.length; i++) {
+        $(sel).append('<div id="bk' + i + '" class="col-sm-6 col-md-4 col-lg-3"></div>');
+        var bieżąca_lista = listy.eq(i);
+        var bieżące_pozycje = bieżąca_lista.children('dt');
+        $('#bk' + i).append('<h4>' + bieżąca_lista.prev().html() + '</h4>');
+        $('#bk' + i).append('<p><dl>');
+        for (var j = 0; j < bieżące_pozycje.length; j++) {
+            var bieżąca_pozycja = bieżące_pozycje.eq(j);
+            var bieżące_linki = bieżąca_pozycja.children('a');
+            if (bieżące_linki.length > 0) {
+                var link = bieżące_linki.first();
+                if (link.attr('ICON_URI') != null)
+                    $('#bk' + i).append('<dt><a href="' + link.attr('href') + '"><img src="' + link.attr('ICON_URI') + '" alt="" title="' + link.text() + '" width="16" height="16"> ' + link.text() + '</a></dt>');
+                else
+                    $('#bk' + i).append('<dt>' + link[0].outerHTML + '</dt>');
+            }
+        }
+        $('#bk' + i).append('</dl></p>');
+    }
+    $(sel).find('a').attr('target', '_blank');
+}
+export function updateMainIcons(sel: string | JQuery<HTMLElement>, html: any): void 
+{
+    $(sel).html('');
+    var links = $('a[icon], a[icon_uri]', html);
+    $(sel).append('<p>');
+    for (var i = 0; i < links.length; i++) {
+        var link = links.eq(i);
+        if (link.attr('ICON_URI') != null)
+            $(sel).append('<a href="' + link.attr('href') + '"><img src="' + link.attr('ICON_URI') + '" alt="' + link.text() + '" title="' + link.text() + '" width="32" height="32"></a> ');
+        else
+            $(sel).append('<a href="' + link.attr('href') + '"><img src="' + link.attr('ICON') + '" alt="' + link.text() + '" title="' + link.text() + '" width="32" height="32"></a> ');
+    }
+    $(sel).append('</p>');
+    $(sel).find('a').attr('target', '_blank');
 }
 export function startMain(href: string) : void
 {
-    start("#main", "#spnnr", href, updateMain)
+    start('#info', '#info', '/info.html', updateMainInfo)
+    start('#icns', '#icns', '/icons.html', updateMainIcons)
+    start('#bks', '#bke', '/bookmarks.html', updateMainBookmarks)
+    $('#google').focus()
+
+    // start("#main", "#spnnr", href, updateMain)
 }
 (<any>window).startMain = startMain;
 export function updateBookmarks(sel: string | JQuery<HTMLElement>, html: any): void 
